1ef7afbada4fb83b9c0f0743a0a4e97ee6420412
Migdał <mmigdal@iimcb.gov.pl>
2019-02-28 12:41:18 +0100

    make combine_pvalues.R more readable

--- combine_pvalues.R
+++ combine_pvalues.R
@@ -1,32 +1,41 @@
+#!/usr/bin/env R
+# Migdal Feb 2019
+# Combine p-values bedgraphs using Fisher method
+# outputs combined bedgraph with p-values corrected using BH method
+# Usage: Rscript combine_pvalues.R out.bedGraph in_1.bedGraph in_2.bedGraph ...
 library(rtracklayer)
 library(GenomicRanges)
 
 args <- commandArgs(TRUE)
-out <- args[1]
-input <- args[-1]
+out <- args[1] # Output file name
+input <- args[-1] # Input bedgraphs
+
+stopifnot(length(input) > 2)
 
 # constants
 e <- exp(1)
-log10e <- log10(e)
+log10e <- log10(e) # const for changing log10 to ln
+K <- 2 * length(input) # degrees of freedom
 
-# sum replicates p-values
-bdg_chi2 <- lapply(input, import, format = "bedGRaph")
-bdg_chi2 <- GRangesList(bdg_chi2)
-bdg_chi2 <- coverage(bdg_chi2, weight = "score")
-bdg_chi2 <- GRanges(bdg_chi2)
+# sum input bedGraphs
+bdg_sum <- lapply(input, import, format = "bedGRaph")
+bdg_sum <- GRangesList(bdg_sum)
+bdg_sum <- coverage(bdg_sum, weight = "score")
+bdg_sum <- GRanges(bdg_sum)
 
 # calculate Fisher's method p-value
-bdg_chi2$score <- 2 * (bdg_chi2$score / log10(e))
-bdg_chi2$score <- pchisq(bdg_chi2$score, df=2 * length(input), lower.tail=FALSE)
-bdg_chi2$score[is.infinite(bdg_chi2$score)] <- 324 # -log10(1e-324) and below return Inf
+# chi2 = 2 * sum(ln(pval_1) + ln(pval_2) + ...)
+bdg_sum$score <- 2 * (bdg_sum$score / log10e)
+bdg_sum$score <- pchisq(bdg_sum$score, df = K, lower.tail=FALSE)
+bdg_sum$score[is.infinite(bdg_sum$score)] <- 324 # -log10(1e-324) and below return Inf
 
 # adjust p-value
-idxes <- 1:length(bdg_chi2) + c(0, cumsum(width(bdg_chi2)[1:(length(bdg_chi2) - 1)]) - 1:(length(bdg_chi2) - 1))
-pvalues <- rep(bdg_chi2$score, times = width(bdg_chi2))
+idxes <- 1:length(bdg_sum) + c(0, cumsum(width(bdg_sum)[1:(length(bdg_sum) - 1)]) - 1:(length(bdg_sum) - 1))
+pvalues <- rep(bdg_sum$score, times = width(bdg_sum))
 qvalues <- p.adjust(pvalues, method = "BH")
 qvalues <- qvalues[idxes]
 qvalues <- -log10(qvalues)
 qvalues[is.infinite(qvalues)] <- 324
-bdg_chi2$score <- qvalues
+bdg_sum$score <- qvalues
 
-export(bdg_chi2, con = out, format = "bedGRaph")
+export(bdg_sum, con = out, format = "bedGRaph")
